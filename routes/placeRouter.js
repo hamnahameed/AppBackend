6b0c966b35a42ecf0021a8402e7ab431a79cccbf
mqumailr@gmail.com <mqumailr@gmail.com>
2024-02-03 13:19:06 +0500

    api

--- routes/placeRouter.js
+++ routes/placeRouter.js
@@ -31,4 +31,69 @@ router.get("/place", requireToken, async (req, res) => {
   }
 });
 
+router.get("/place/:placeId", requireToken, async (req, res) => {
+  try {
+    Place.findOne({_id: req.params.placeId}).then((place) => {
+      if(place){
+        res.status(200).send({ place });
+      }
+      else {
+        res.status(422).send({error: "place not found"})
+      }
+    });
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
+router.post("/place/mechanic/:placeId", requireToken, async (req, res) => {
+  try {
+    const {placeId} = req.params;
+    const {mechanic} = req.body;
+    console.log('mechanic', mechanic)
+    Place.findByIdAndUpdate(placeId, 
+      {$push: {nearestMechanic: mechanic}}).then(result => {
+        if(result){
+          return res.status(200).send({msg: 'mechanic added successfully!'})
+        }
+        else return res.status(422).send({error: 'something went wrong'})
+      })
+  } catch (error) {
+    res.status(500).send({error: error?.message})
+  }
+})
+
+router.delete('/place/:placeId', requireToken, async(req, res) => {
+  try {
+      Place.findOne({_id: req.params.placeId}).then((place) => {
+        if(!place){
+          return res.status(422).send({error: "Already deleted place!"})
+        }
+        place.deleteOne().then(result => {
+          res.status(200).send({msg: 'place deleted successfully'})
+        }).catch(err => {
+          res.status(500).send({error: err.message})
+        })
+      })
+  }
+  catch(err) {
+    res.status(422).send({error:err.message})
+  }
+})
+
+router.delete('/place/mechanic/:placeId/:mechanicId', requireToken, async(req, res) => {
+  try {
+    const {placeId, mechanicId} = req.params;
+    Place.findByIdAndUpdate(placeId, 
+      { $pull: { nearestMechanic: [mechanicId] } },
+      { new: true }
+      ).then(result => {console.log('rr', result)
+    return res.status(200).send({data: result})
+    })
+  }
+  catch(err) {
+    res.status(422).send({error:err.message})
+  }
+})
+
 module.exports = router;
